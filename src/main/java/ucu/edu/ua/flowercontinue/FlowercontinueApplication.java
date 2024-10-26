820bb511d9f5b915556314dd6b02bd3006dc1c99
Anna-Mariia <vitkovska.pn@ucu.edu.ua>
2024-10-26 22:59:32 +0300

    Adding all initial Flower files with web

---
+++ src/main/java/ucu/edu/ua/flowercontinue/FlowercontinueApplication.java
@@ -0,0 +1,15 @@
+package ucu.edu.ua.flowercontinue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class FlowercontinueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowercontinueApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ucu/edu/ua/flowercontinue/flowers/Item.java
@@ -0,0 +1,5 @@
+package ucu.edu.ua.flowercontinue.flowers;
+
+public abstract class Item {
+    public abstract double getPrice();
+}
